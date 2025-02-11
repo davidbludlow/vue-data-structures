65dbe5c62d90345f9133450ecdb393616991e620
David B Ludlow <5985136+davidbludlow@users.noreply.github.com>
2025-02-11 14:23:30 -0700

    Added `useCachedNonReactiveWrappers`

--- src/use-cached-non-reactive-wrappers.ts
+++ src/use-cached-non-reactive-wrappers.ts
@@ -1,45 +1,33 @@
 // This file was copied from
-// https://github.com/davidbludlow/vue-data-structures/blob/main/src/use-cached-wrappers.ts
+// https://github.com/davidbludlow/vue-data-structures/blob/main/src/use-cached-non-reactive-wrappers.ts
 // which had an MIT license, when it was copied.
 
-import { type Reactive, reactive, type UnwrapNestedRefs } from 'vue';
-
-/** Creates data wrapper provider. When run, a data wrapper provider will create
- * a data wrapper for a given `data` object, or, if a data wrapper has already
- * been created for that `data` then it will return the previously created data
- * wrapper. `data` should be the information that needs serialization only.
- * `factory` should be a function to create new data wrappers.
+/** Most of the time, in a Vue program, do not use this and use
+ * `useCachedWrappers` instead.
  *
- * See https://github.com/davidbludlow/vue-data-structures/#useCachedWrappers
- * for more information. */
-export function useCachedWrappers<
-  TData extends UnwrapNestedRefs<object>,
+ * Creates a non-reactive data wrapper provider. When run, a data wrapper
+ * provider will create a data wrapper for a given `data` object, or, if a data
+ * wrapper has already been created for that `data` then it will return the
+ * previously created data wrapper. `data` should be the information that needs
+ * serialization only. `factory` should be a function to create new data
+ * wrappers.
+ *
+ * This is just like
+ * https://github.com/davidbludlow/vue-data-structures/#useCachedWrappers except
+ * that it is very slightly faster by skipping Vue reactivity. */
+export function useCachedNonReactiveWrappers<
+  TData extends object,
   TWrapper extends object,
   TAdditionalParams extends any[],
 >(
   factory: (data: TData, ...additionalParams: TAdditionalParams) => TWrapper,
-): (data: TData, ...additionalParams: TAdditionalParams) => Reactive<TWrapper> {
-  // Do not worry about the performance of `WeakMap`. Vue already uses `WeakMap`
-  // extremely frequently (like every time you use a reactive object).
-  const cache = new WeakMap<TData, Reactive<TWrapper>>();
-  return (
-    data: TData,
-    ...additionalParams: TAdditionalParams
-  ): Reactive<TWrapper> => {
-    /** `reactiveData === data` will be true if `data` was already reactive.
-     * (Vue 3 internally uses `WeakMap` to cache reactive `Proxy`s to make that
-     * possible.) */
-    const reactiveData = reactive(data) as TData;
-    const cached = cache.get(reactiveData);
+): (data: TData, ...additionalParams: TAdditionalParams) => TWrapper {
+  const cache = new WeakMap<TData, TWrapper>();
+  return (data: TData, ...additionalParams: TAdditionalParams): TWrapper => {
+    const cached = cache.get(data);
     if (cached) return cached;
-    const factoryOutput = factory(reactiveData, ...additionalParams);
-    // Calling `reactive()` on it will make it so you do not need to call
-    // `.value` on the refs and computed. (See
-    // https://vuejs.org/api/reactivity-core.html#reactive for proof of that.)
-    // BTW, `wrapper === factoryOutput` will already be true if you used
-    // `createAugmentingWrapperFactory()` to create `factory`.
-    const wrapper = reactive(factoryOutput);
-    cache.set(reactiveData, wrapper);
+    const wrapper = factory(data, ...additionalParams);
+    cache.set(data, wrapper);
     return wrapper;
   };
 }
